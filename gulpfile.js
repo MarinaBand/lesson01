93f3ee6689393fb6c3e261e792651e480c72e649
Marina Bandura <marina.caramel@gmail.com>
2021-09-16 17:55:11 +0300

    added navbar, gulp.tasks: watch, webserver

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ const gulp = require('gulp'),
     prefixer = require('gulp-autoprefixer'),
     htmlmin = require('gulp-htmlmin'),
     terser = require('gulp-terser'),
-    rigger = require('gulp-rigger');
+    rigger = require('gulp-rigger'),
+    rimraf = require('rimraf'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+const {stream} = require("browser-sync");
 
 // описание путей
 const path = {
@@ -24,14 +28,42 @@ const path = {
         fonts:'src/fonts/**/*.{eot,svg,ttf,woff,woff2}',
         img:'src/img/**/*.{jpg,gif,jpeg,png,svg,webp}',
     },
-
+    watch:{
+        html:'src/index.html',
+        scss:'src/scss/**/*.scss',
+        js:'src/js/**/*.js',
+        fonts:'src/fonts/**/*.{eot,svg,ttf,woff,woff2}',
+        img:'src/img/**/*.{jpg,gif,jpeg,png,svg,webp}'
+    },
+    clean:'build/'
+};
+//Описание конфигурации
+const config = {
+    server:{
+        baseDir:'build/',
+        index:'index.html',
+    },
+    tunnel:true,
+    port:4201,
+    logPrefix: 'WebDev'
 
 };
-
 // Описания задач
+gulp.task('clean', function(done){
+    rimraf(path.clean, done);
+});
+
 gulp.task('mv:fonts', function(done){
     gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts));
+        .pipe(gulp.dest(path.build.fonts))
+        .pipe(reload({stream: true}));
+    done();
+});
+
+gulp.task('mv:img', function(done){
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
     done();
 });
 
@@ -43,7 +75,8 @@ gulp.task('build:html', function(done){
             removeComments: true,
             useShortDoctype: true
         }))
-        .pipe(gulp.dest(path.build.html));
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
     done();
 });
 
@@ -57,7 +90,8 @@ gulp.task('build:scss', function(done){
             cascade: false,
             remove: true
         }))
-        .pipe(gulp.dest(path.build.scss));
+        .pipe(gulp.dest(path.build.scss))
+        .pipe(reload({stream: true}));
     done();
 });
 
@@ -66,11 +100,22 @@ gulp.task('build:js', function(done){
         .pipe(plumber())
         .pipe(rigger())
         .pipe(terser())
-        .pipe(gulp.dest(path.build.js));
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+    done();
+});
+gulp.task('watch', function (done) {
+    gulp.watch(path.watch.html, gulp.series('build:html'));
+    gulp.watch(path.watch.scss, gulp.series('build:scss'));
+    gulp.watch(path.watch.js, gulp.series('build:js'));
+    gulp.watch(path.watch.fonts, gulp.series('mv:fonts'));
+    gulp.watch(path.watch.img, gulp.series('mv:img'));
+    done();
+});
+
+gulp.task('webserver', function(done){
+    browserSync(config);
     done();
 });
 // Основная задача
-gulp.task('default', gulp.parallel('mv:fonts','build:html','build:scss','build:js'));
-
-
-
+gulp.task('default', gulp.series('clean',gulp.parallel('mv:fonts','mv:img','build:html','build:scss','build:js'),'watch','webserver'));
